db1882dd07a3ffb78693a9c3a613721146a9fe64
unknown <brettmcadams96@gmail.com>
2020-11-16 15:35:21 -0500

    seeds passwords now hashed

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,16 +1,21 @@
+const bcryptjs = require("bcryptjs");
+
 exports.seed = function (knex) {
   // 000-cleanup.js already cleaned out all tables
+
+  const password1 = bcryptjs.hashSync("password", 8);
+  const password2 = bcryptjs.hashSync("password", 8);
 
   const users = [
     {
       name: "admin",
       email: "admin@admin.com",
-      password: "password",
+      password: password1,
     },
     {
       name: "user",
       email: "user@user.com",
-      password: "password",
+      password: password2,
     },
   ];
 
