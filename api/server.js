af44f666edf2ac8db85990e879f6ef887dc097ed
unknown <brettmcadams96@gmail.com>
2020-11-17 01:38:22 -0500

    added doc redirect

--- api/server.js
+++ api/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const usersRouter = require('../users/users-router')
-const postsRouter = require('../posts/posts-router')
-const authRouter = require('../auth/auth-router')
+const usersRouter = require("../users/users-router");
+const postsRouter = require("../posts/posts-router");
+const authRouter = require("../auth/auth-router");
 
 const server = express();
 
@@ -12,12 +12,12 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/users', usersRouter)
-server.use('/api/posts', postsRouter)
-server.use('/api/auth', authRouter)
+server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
+server.use("/api/auth", authRouter);
 
 server.get("*", (req, res) => {
-  res.json({ api: "up and runnning" });
+  res.redirect("https://documenter.getpostman.com/view/12353263/TVeqbmjL");
 });
 
 module.exports = server;
